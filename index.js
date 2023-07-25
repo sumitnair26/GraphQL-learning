6369c149638303864a3a13c979c7f6f9e990fc2e
sumitnair26 <sumit.nair26@gmail.com>
2023-07-25 21:01:04 +0530

    type defination schema.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+//types
+
+import { typeDefs } from './schema.js';
+
 //server setup
 const server = new ApolloServer({
     //typeDefs (Datatype and relationship with other data type )
+    /* deefination of type od data */
+    typeDefs,
     //resolver
 })
 
---
+++ schema.js
@@ -0,0 +1,34 @@
+/*
+ ! denotes Mandatory 
+
+*/
+
+export const typeDefs = `#graphql
+type Game {
+    id: ID!, 
+    title: String!,
+    platform: [String!]!
+}
+type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+}
+
+type Authoe {
+    id: ID!
+    name: String!
+    verified: Boolean!
+}
+
+type Query {
+    reviews: [Reviews]
+    games: [Game]
+    authors: [Author]
+}
+
+`
+
+
+
+
